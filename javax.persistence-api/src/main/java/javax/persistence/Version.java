2ff01b6026976130bab168f93dca3d60b165030e
Satoshi Kubo <skubo@bbs.co.jp>
2017-11-10 14:12:34 +0900

    translate.

--- javax.persistence-api/src/main/java/javax/persistence/Version.java
+++ javax.persistence-api/src/main/java/javax/persistence/Version.java
@@ -22,24 +22,15 @@ import static java.lang.annotation.ElementType.FIELD;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
 /**
- * Specifies the version field or property of an entity class that
- * serves as its optimistic lock value.  The version is used to ensure
- * integrity when performing the merge operation and for optimistic
- * concurrency control.
- *
- * <p> Only a single <code>Version</code> property or field 
- * should be used per class; applications that use more than one 
- * <code>Version</code> property or field will not be portable. 
+ * 楽観ロック値として機能するエンティティクラスの<code>Version</code>フィールドまたはプロパティを指定します。
+ * この<code>Version</code>は、マージ操作を実行する際の整合性と楽観ロックによる並行性制御のために使用されます。
  * 
- * <p> The <code>Version</code> property should be mapped to 
- * the primary table for the entity class; applications that 
- * map the <code>Version</code> property to a table other than 
- * the primary table will not be portable.
+ * <p>クラスごとに1つの<code>Version</code>プロパティまたはフィールドのみ使用できます。複数の<code>Version</code>プロパティまたはフィールドを使用するアプリケーションは移植できないでしょう。
  * 
- * <p> The following types are supported for version properties: 
- * <code>int</code>, <code>Integer</code>, <code>short</code>, 
- * <code>Short</code>, <code>long</code>, <code>Long</code>, 
- * <code>java.sql.Timestamp</code>.
+ * <p><code>Version</code>プロパティは、エンティティクラスのプライマリテーブルに設置する必要があります。
+ * <code>Version</code>プロパティをプライマリテーブル以外のテーブルに設置するアプリケーションは移植できないでしょう。
+ * 
+ * <p><code>Version</code>プロパティでサポートされる型は<code>int</code>、<code>Integer</code>、<code>short</code>、<code>Short</code>、<code>long</code>、<code>Long</code>、<code>java.sql.Timestamp</code>です。
  *
  * <pre>
  *    Example:
