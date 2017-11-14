18a71db3b69b11831c9c36790d0f16e221704211
Satoshi Kubo <megascus@gmail.com>
2017-11-15 08:27:06 +0900

    translate.

--- javax.persistence-api/src/main/java/javax/persistence/Query.java
+++ javax.persistence-api/src/main/java/javax/persistence/Query.java
@@ -24,7 +24,7 @@ import java.util.Map;
 import java.util.stream.Stream;
 
 /**
- * Interface used to control query execution.
+ * クエリー実行のコントロールに使用するインターフェースです。
  *
  * @see TypedQuery
  * @see StoredProcedureQuery
