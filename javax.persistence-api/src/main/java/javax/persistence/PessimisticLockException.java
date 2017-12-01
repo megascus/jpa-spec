b05f97a0463211c85c26f8d2b0ae63c373905d81
Satoshi Kubo <megascus@gmail.com>
2017-12-01 10:03:32 +0900

    translate.

--- javax.persistence-api/src/main/java/javax/persistence/PessimisticLockException.java
+++ javax.persistence-api/src/main/java/javax/persistence/PessimisticLockException.java
@@ -16,68 +16,62 @@
 package javax.persistence;
 
 /**
- * Thrown by the persistence provider when an pessimistic locking conflict
- * occurs. This exception may be thrown as part of an API call, a flush or at 
- * commit time. The current transaction, if one is active, will be marked for 
- * rollback.
+ * 悲観ロックでの競合が発生した場合に永続化プロバイダーによって投げられます。
+ * 
+ * この例外はコミット時のフラッシュAPI呼び出しの一部で投げられるでしょう。
+ * 現在のトランザクションがアクティブである場合はロールバックするようにマークされます。
  *
  * @since Java Persistence 2.0
  */
 public class PessimisticLockException extends PersistenceException {
-    /** The object that caused the exception */
+    /** 例外の原因となったオブジェクト */
     Object entity;
 
     /** 
-     * Constructs a new <code>PessimisticLockException</code> exception 
-     * with <code>null</code> as its detail message.
+     * 新しい<code>PessimisticLockException</code>例外を<code>null</code>を詳細メッセージとして生成します。
      */
     public PessimisticLockException() {
         super();
     }
 
     /** 
-     * Constructs a new <code>PessimisticLockException</code> exception 
-     * with the specified detail message.
-     * @param   message   the detail message.
+     * 新しい<code>PessimisticLockException</code>例外を指定された詳細メッセージで生成します。
+     * @param   message   詳細メッセージ
      */
     public PessimisticLockException(String message) {
         super(message);
     }
 
     /** 
-     * Constructs a new <code>PessimisticLockException</code> exception 
-     * with the specified detail message and cause.
-     * @param   message   the detail message.
-     * @param   cause     the cause.
+     * 新しい<code>PessimisticLockException</code>例外を指定された詳細メッセージと原因で生成します。
+     * @param   message   詳細メッセージ
+     * @param   cause     原因
      */
     public PessimisticLockException(String message, Throwable cause) {
         super(message, cause);
     }
 
     /** 
-     * Constructs a new <code>PessimisticLockException</code> exception 
-     * with the specified cause.
-     * @param   cause     the cause.
+     * 新しい<code>PessimisticLockException</code>例外を指定された原因で生成します。
+     * @param   cause     原因
      */
     public PessimisticLockException(Throwable cause) {
         super(cause);
     }
 
     /** 
-     * Constructs a new <code>PessimisticLockException</code> exception 
-     * with the specified entity.
-     * @param   entity     the entity.
+     * 新しい<code>PessimisticLockException</code>例外を指定されたエンティティで生成します。
+     * @param   entity     エンティティ
      */
     public PessimisticLockException(Object entity) {
         this.entity = entity;
     }
 
     /** 
-     * Constructs a new <code>PessimisticLockException</code> exception 
-     * with the specified detail message, cause, and entity.
-     * @param   message   the detail message.
-     * @param   cause     the cause.
-     * @param   entity     the entity.
+     * 新しい<code>PessimisticLockException</code>例外を指定された詳細メッセージと原因とエンティティで生成します。
+     * @param   message   詳細メッセージ
+     * @param   cause     原因
+     * @param   entity     エンティティ
      */
     public PessimisticLockException(String message, Throwable cause, Object entity) {
         super(message, cause);
@@ -85,8 +79,8 @@ public class PessimisticLockException extends PersistenceException {
     }
     
     /**
-     * Returns the entity that caused this exception.
-     * @return the entity.
+     * この例外の原因となったエンティティを返します。
+     * @return エンティティ
      */
     public Object getEntity() {
         return this.entity;
