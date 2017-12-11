d0ae5c856d145507a431b93b7991ed9371f85f5c
Satoshi Kubo <megascus@gmail.com>
2017-12-11 21:05:48 +0900

    translate.

--- javax.persistence-api/src/main/java/javax/persistence/OneToOne.java
+++ javax.persistence-api/src/main/java/javax/persistence/OneToOne.java
@@ -24,28 +24,20 @@ import static java.lang.annotation.RetentionPolicy.RUNTIME;
 import static javax.persistence.FetchType.EAGER;
 
 /**
- * Specifies a single-valued association to another entity that has
- * one-to-one multiplicity. It is not normally necessary to specify
- * the associated target entity explicitly since it can usually be
- * inferred from the type of the object being referenced.  If the relationship is
- * bidirectional, the non-owning side must use the <code>mappedBy</code> element of
- * the <code>OneToOne</code> annotation to specify the relationship field or
- * property of the owning side.
- *
- * <p> The <code>OneToOne</code> annotation may be used within an
- * embeddable class to specify a relationship from the embeddable
- * class to an entity class. If the relationship is bidirectional and
- * the entity containing the embeddable class is on the owning side of
- * the relationship, the non-owning side must use the
- * <code>mappedBy</code> element of the <code>OneToOne</code>
- * annotation to specify the relationship field or property of the
- * embeddable class. The dot (".") notation syntax must be used in the
- * <code>mappedBy</code> element to indicate the relationship attribute within the
- * embedded attribute.  The value of each identifier used with the dot
- * notation is the name of the respective embedded field or property.
+ * 1対1の多重度を持つ別のエンティティへの単一値の関連付けを指定します。
+ * 
+ * 通常は関連するターゲットエンティティは参照されているオブジェクトの型から推論できるため明示的に指定する必要はありません。
+ * 関係が双方向である場合、被所有側のエンティティは<code>OneToOne</code>アノテーションの<code>mappedBy</code>要素を使用して、
+ * 所有側のエンティティの関係を示すフィールドまたはプロパティを指定する必要があります。
+ * 
+ * <p> <code>OneToOne</code>アノテーションは組み込みクラス({@link Embeddable}の付いたクラス)からエンティティクラスへの関係を指定するために組み込みクラス内で使用できます。
+ * 関係が双方向であり、組み込みクラスを含むエンティティが関係の所有側のエンティティである場合、
+ * 被所有側のエンティティでは、組み込みクラスの関係を示すフィールドまたはプロパティを指定するために<code>mappedBy</code>アノテーションの<code>mappedBy</code>要素を使用する必要があります。
+ * 組み込み属性内のリレーションシップ属性を示すには、<code>mappedBy</code>要素でドット(".")表記を使用する必要があります。
+ * ドット表記で使用される各識別子の値は、それぞれの組み込みフィールドまたはプロパティの名前です。
  * 
  * <pre>
- *    Example 1: One-to-one association that maps a foreign key column
+ *    Example 1: 外部キー列を使用してマッピングする1対1の関連付け
  *
  *    // On Customer class:
  *
@@ -60,7 +52,7 @@ import static javax.persistence.FetchType.EAGER;
  *    public Customer getCustomer() { return customer; }
  *
  *
- *    Example 2: One-to-one association that assumes both the source and target share the same primary key values. 
+ *    Example 2: ソースとターゲットの両方が同じ主キー値を共有することを前提とした1対1の関連付け
  *
  *    // On Employee class:
  *
@@ -82,7 +74,7 @@ import static javax.persistence.FetchType.EAGER;
  *    }
  *
  *
- *    Example 3: One-to-one association from an embeddable class to another entity.
+ *    Example 3: 組み込みクラスから別のエンティティへの1対1の関連付け
  *
  *    &#064;Entity
  *    public class Employee {
@@ -116,48 +108,45 @@ import static javax.persistence.FetchType.EAGER;
 public @interface OneToOne {
 
     /** 
-     * (Optional) The entity class that is the target of 
-     * the association. 
+     * (オプション) 関連付けの対象となるエンティティクラス。
      *
-     * <p> Defaults to the type of the field or property 
-     * that stores the association. 
+     * <p> デフォルトでは関連付けを格納するフィールドまたはプロパティの型になります。
      */
     Class targetEntity() default void.class;
 
     /**
-     * (Optional) The operations that must be cascaded to 
-     * the target of the association.
+     * (オプション) 関連付けのターゲットにカスケードする必要のある操作。
      *
-     * <p> By default no operations are cascaded.
+     * <p> デフォルトではすべての操作はカスケードされません。
      */
     CascadeType[] cascade() default {};
 
     /** 
-     * (Optional) Whether the association should be lazily 
-     * loaded or must be eagerly fetched. The EAGER 
-     * strategy is a requirement on the persistence provider runtime that 
-     * the associated entity must be eagerly fetched. The LAZY 
-     * strategy is a hint to the persistence provider runtime.
+     * (オプション) 関連付けを遅延ロード(LAZY)するか、即座に取得(EAGER)する必要があるかどうか。
+     * 
+     * EAGER戦略は永続性プロバイダーの実行時に関連エンティティを即座に取得するべきだとする要件です。
+     * LAZY戦略は持続性プロバイダーの実行時のヒントです。
+     * 
+     * <p>訳注：つまりEAGERは必ず有効になり、LAZYが有効になるかは実装に依存します。
      */
     FetchType fetch() default EAGER;
 
     /** 
-     * (Optional) Whether the association is optional. If set 
-     * to false then a non-null relationship must always exist.
+     * (オプション) 関連付けがオプションかどうか。
+     * 
+     * もしfalseである場合、この関連は常にnullでない必要があります。
      */
     boolean optional() default true;
 
-    /** (Optional) The field that owns the relationship. This 
-      * element is only specified on the inverse (non-owning) 
-      * side of the association.
+    /** (オプション) 関係を所有するフィールド。
+     * 
+     * この要素は、関連の逆(被所有)側でのみ指定されます。
      */
     String mappedBy() default "";
 
 
     /**
-     * (Optional) Whether to apply the remove operation to entities that have
-     * been removed from the relationship and to cascade the remove operation to
-     * those entities.
+     * (オプション) 関係から削除されたエンティティに対し削除操作を適用し削除操作をそれらのエンティティにカスケードするかどうか。
      * @since Java Persistence 2.0
      */
     boolean orphanRemoval() default false;
