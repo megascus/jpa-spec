ef642a9a0a4262b9b5613180e98ca5e28de5f2d3
Satoshi Kubo <megascus@gmail.com>
2018-01-03 12:09:12 +0900

    translate.

--- javax.persistence-api/src/main/java/javax/persistence/GenerationType.java
+++ javax.persistence-api/src/main/java/javax/persistence/GenerationType.java
@@ -16,7 +16,7 @@
 package javax.persistence;
 
 /** 
- * Defines the types of primary key generation strategies. 
+ * 主キー生成戦略の型を定義します。
  *
  * @see GeneratedValue
  *
@@ -25,32 +25,26 @@ package javax.persistence;
 public enum GenerationType { 
 
     /**
-     * Indicates that the persistence provider must assign 
-     * primary keys for the entity using an underlying 
-     * database table to ensure uniqueness.
+     * 永続性プロバイダは一意性を保証するために基になるデータベースのテーブルを使用してエンティティの主キーに値を割り当てなければならないことを示します。
      */
     TABLE, 
 
     /**
-     * Indicates that the persistence provider must assign 
-     * primary keys for the entity using a database sequence.
+     * 永続性プロバイダはデータベースのシーケンスを使用してエンティティの主キーに値を割り当てなければならないことを示します。
      */
     SEQUENCE, 
 
     /**
-     * Indicates that the persistence provider must assign 
-     * primary keys for the entity using a database identity column.
+     * 永続性プロバイダはデータベースのID列を使用してエンティティの主キーに値を割り当てなければならないことを示します。
      */
     IDENTITY, 
 
     /**
-     * Indicates that the persistence provider should pick an 
-     * appropriate strategy for the particular database. The 
-     * <code>AUTO</code> generation strategy may expect a database 
-     * resource to exist, or it may attempt to create one. A vendor 
-     * may provide documentation on how to create such resources 
-     * in the event that it does not support schema generation 
-     * or cannot create the schema resource at runtime.
+     * 永続性プロバイダが特定のデータベースに対して適切な戦略を選択しなければならないことを示します。
+     * 
+     * <code>AUTO</code>生成戦略ではデータベースリソースが存在するか、生成されることが期待されます。
+     * ベンダーはスキーマの生成をサポートしていない場合、または実行時にスキーマリソースを生成できない場合に、
+     * そのようなリソースを生成する方法に関するドキュメントを提供することがあります。
      */
     AUTO
 }
