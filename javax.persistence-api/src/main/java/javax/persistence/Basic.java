2aba8abe8ab5c8480d25ebe87843110e60980ad1
Satoshi Kubo <megascus@gmail.com>
2018-01-18 13:25:46 +0900

    translate.

--- javax.persistence-api/src/main/java/javax/persistence/Basic.java
+++ javax.persistence-api/src/main/java/javax/persistence/Basic.java
@@ -23,25 +23,21 @@ import static java.lang.annotation.RetentionPolicy.RUNTIME;
 import static javax.persistence.FetchType.EAGER;
 
 /**
- * The simplest type of mapping to a database column. The
- * <code>Basic</code> annotation can be applied to a persistent
- * property or instance variable of any of the following types: Java
- * primitive types, wrappers of the primitive types, <code>String</code>, 
- * <code>java.math.BigInteger</code>,
- * <code>java.math.BigDecimal</code>,
- * <code>java.util.Date</code>,
- * <code>java.util.Calendar</code>, 
- * <code>java.sql.Date</code>, 
- * <code>java.sql.Time</code>,
- * <code>java.sql.Timestamp</code>, <code>byte[]</code>, <code>Byte[]</code>,
- * <code>char[]</code>, <code>Character[]</code>, enums, and any other type that
- * implements <code>java.io.Serializable</code>.
+ * データベース列へのマッピングのもっとも単純な型です。
  * 
- * <p> The use of the <code>Basic</code> annotation is optional for
- * persistent fields and properties of these types.  If the
- * <code>Basic</code> annotation is not specified for such a field or
- * property, the default values of the <code>Basic</code> annotation
- * will apply.
+ * <code>Basic</code>アノテーションは次の型のフィールドもしくはプロパティに適用することができます。:
+ * Javaプリミティブ型,、プリミティブ型のラッパー、 <code>String</code>、 
+ * <code>java.math.BigInteger</code>、
+ * <code>java.math.BigDecimal</code>、
+ * <code>java.util.Date</code>、
+ * <code>java.util.Calendar</code>、 
+ * <code>java.sql.Date</code>、 
+ * <code>java.sql.Time</code>、
+ * <code>java.sql.Timestamp</code>、 <code>byte[]</code>、 <code>Byte[]</code>、
+ * <code>char[]</code>、 <code>Character[]</code>、列挙型、<code>java.io.Serializable</code>を実装したほかのすべての型。
+ * 
+ * <p> これらの型の永続化フィールドもしくはプロパティに対する<code>Basic</code>アノテーションの使用はオプションです。
+ * <code>Basic</code>アノテーションがそのようなフィールドもしくはプロパティに指定されていない場合は<code>Basic</code>アノテーションのデフォルトの値が適用されます。
  *
  * <pre>
  *    Example 1:
@@ -62,20 +58,19 @@ import static javax.persistence.FetchType.EAGER;
 public @interface Basic {
 
     /**
-     * (Optional) Defines whether the value of the field or property should 
-     * be lazily loaded or must be eagerly fetched. The <code>EAGER</code> 
-     * strategy is a requirement on the persistence provider runtime 
-     * that the value must be eagerly fetched.  The <code>LAZY</code> 
-     * strategy is a hint to the persistence provider runtime.
-     * If not specified, defaults to <code>EAGER</code>.
+     * (オプション) フィールドもしくはプロパティを遅延ロード(LAZY)すべきか、即座に取得(EAGER)する必要があるかどうか。
+     * 
+     * <code>EAGER</code>戦略は永続化プロバイダの実行時に関連エンティティを即座に取得するべきだとする要件です。
+     * <code>LAZY</code> 戦略は永続化プロバイダの実行時のヒントです。
+     * 指定されていない場合のデフォルトは<code>EAGER</code>です。
      */
     FetchType fetch() default EAGER;
 
     /**
-     * (Optional) Defines whether the value of the field or property may be null. 
-     * This is a hint and is disregarded for primitive types; it may 
-     * be used in schema generation.
-     * If not specified, defaults to <code>true</code>.
+     * (オプション) フィールドもしくはプロパティの値がnullになりえるかどうかを定義します。
+     * 
+     * これはヒントであり、プリミティブ型では無視されます。これはスキーマ生成で使用されます。
+     * 指定されていない場合のデフォルト値は<code>true</code>です。
      */
     boolean optional() default true;
 }
