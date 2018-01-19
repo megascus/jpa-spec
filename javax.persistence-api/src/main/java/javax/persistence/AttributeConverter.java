e1cdfdf542f0bf3ababb4d00f39aa0b76c62f01a
Satoshi Kubo <megascus@gmail.com>
2018-01-19 10:52:46 +0900

    translate.

--- javax.persistence-api/src/main/java/javax/persistence/AttributeConverter.java
+++ javax.persistence-api/src/main/java/javax/persistence/AttributeConverter.java
@@ -18,35 +18,29 @@ package javax.persistence;
  * A class that implements this interface can be used to convert 
  * entity attribute state into database column representation 
  * and back again.
- * Note that the X and Y types may be the same Java type.
+ * XとYの型は同じJavaの型である可能性があることに注意してください。
  *
- * @param <X>  the type of the entity attribute
- * @param <Y>  the type of the database column
+ * @param <X>  エンティティの属性の型
+ * @param <Y>  データベースの列の型
  */
 public interface AttributeConverter<X,Y> {
 
     /**
-     * Converts the value stored in the entity attribute into the 
-     * data representation to be stored in the database.
+     * エンティティの属性に格納されている値をデータ表現に変換してデータベースに格納します。
      *
-     * @param attribute  the entity attribute value to be converted
-     * @return  the converted data to be stored in the database 
-     *          column
+     * @param attribute  変換されるエンティティの属性値
+     * @return  データベースの列に格納するために変換されたデータ
      */
     public Y convertToDatabaseColumn (X attribute);
 
     /**
-     * Converts the data stored in the database column into the 
-     * value to be stored in the entity attribute.
-     * Note that it is the responsibility of the converter writer to
-     * specify the correct <code>dbData</code> type for the corresponding 
-     * column for use by the JDBC driver: i.e., persistence providers are 
-     * not expected to do such type conversion.
+     * データベース列のに格納されたデータをエンティティの属性に格納される値に変換します。
+     * 
+     * JDBCドライバによって使用される対応する列の<code>dbData</code>の正しい型を指定するのはコンバーターライターの責任であることに注意してください。
+     * つまり、永続化プロバイダがそのような型変換を行うことは期待されません。
      *
-     * @param dbData  the data from the database column to be 
-     *                converted
-     * @return  the converted value to be stored in the entity 
-     *          attribute
+     * @param dbData  変換されるデータベース列からのデータ
+     * @return  エンティティの属性に格納するために変換された値
      */
     public X convertToEntityAttribute (Y dbData);
 }
