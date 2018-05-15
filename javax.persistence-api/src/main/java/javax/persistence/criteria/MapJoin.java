678766288d9e3557bc9ad58dec117fceb6729023
Satoshi Kubo <megascus@gmail.com>
2018-05-15 19:52:15 +0900

    translate.

--- javax.persistence-api/src/main/java/javax/persistence/criteria/MapJoin.java
+++ javax.persistence-api/src/main/java/javax/persistence/criteria/MapJoin.java
@@ -19,13 +19,11 @@ import java.util.Map;
 import javax.persistence.metamodel.MapAttribute;
 
 /**
- * The <code>MapJoin</code> interface is the type of the result of
- * joining to a collection over an association or element 
- * collection that has been specified as a <code>java.util.Map</code>.
+ * <code>MapJoin</code>インターフェイスは<code>java.util.Map</code>として指定された関連や要素コレクションを介してコレクションに結合した結果の型です。
  *
- * @param <Z> the source type of the join
- * @param <K> the type of the target Map key
- * @param <V> the type of the target Map value
+ * @param <Z> 結合のソースの型
+ * @param <K> ターゲットMapのキーの型
+ * @param <V> ターゲットMapの値の型
  *
  * @since Java Persistence 2.0
  */
@@ -33,29 +31,28 @@ public interface MapJoin<Z, K, V>
 		extends PluralJoin<Z, Map<K, V>, V> {
 
     /**
-     *  Modify the join to restrict the result according to the
-     *  specified ON condition and return the join object.  
-     *  Replaces the previous ON condition, if any.
-     *  @param restriction  a simple or compound boolean expression
-     *  @return the modified join object
+     * 結合を変更して指定されたON条件に従って結果を制限した結合オブジェクトを戻します。
+     * 
+     * 以前のON条件があれば置き換えます。
+     *  @param restriction  単純な、もしくは複合したブール式
+     *  @return 変更された結合オブジェクト
      *  @since Java Persistence 2.1
      */
     MapJoin<Z, K, V> on(Expression<Boolean> restriction);
 
     /**
-     *  Modify the join to restrict the result according to the
-     *  specified ON condition and return the join object.  
-     *  Replaces the previous ON condition, if any.
-     *  @param restrictions  zero or more restriction predicates
-     *  @return the modified join object
+     * 結合を変更して指定されたON条件に従って結果を制限した結合オブジェクトを戻します。
+     * 
+     * 以前のON条件があれば置き換えます。
+     *  @param restrictions  0個以上の制限の述語
+     *  @return 変更された結合オブジェクト
      *  @since Java Persistence 2.1
      */
     MapJoin<Z, K, V> on(Predicate... restrictions);
 
     /**
-     * Return the metamodel representation for the map attribute.
-     * @return metamodel type representing the <code>Map</code> that is
-     *         the target of the join
+     * マップ属性のメタモデル表現を返します。
+     * @return 結合のターゲットである<code>Map</code>を表すメタモデル型
      */
     MapAttribute<? super Z, K, V> getModel();
     
